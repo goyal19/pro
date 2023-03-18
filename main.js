ca7235031957504c57deb1caf82064b2d99a11bc
goyal19 <101124248+goyal19@users.noreply.github.com>
2023-03-18 14:14:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function preload(){
+
+}
+
+function draw(){
+    Image(video, 230,150,220,200)
+
+    Fill(255,0,0);
+    stroke(255,0,0);
+    circle(50,50,80);
+
+    cirlce(50,430,10);
+
+    circle(590,50,80);
+
+    circle(590,480,30);
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    Rect(90,40,460,20);
+
+    rect(90,420,460,20);
+
+    rect(40,90,20,300);
+
+    rect(580,90,20,300);
+}
+function Take_snapshot(){
+    save('aaryaman.png');
+}
+    
+
+    
+
+
